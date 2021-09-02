4f2841c61758f1d0c3c5b6cca2b2e3bf1cac9c6f
byootan <byootan.tech@gmail.com>
2021-09-02 19:06:29 +0200

    add module sale brand

---
+++ sale_brand_products/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Product Brand',
+    'version': '13.0',
+    'author': 'Byootan Tech',
+    'category': 'Product Management',
+    'license': 'AGPL-3',
+    'summary': """Add Brand For Sales Analysis Report """,
+    'depends': ['brand_products', 'sale_management'],
+    'data': [
+        'reports/sale_report.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
